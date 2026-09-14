2af25238e27abd9c4647446820f68837f54e11a8
Morxidia <xakyazx@gmail.com>
2026-09-14 15:07:50 +0700

    pertemuan_3

--- IF-B/Pertemuan_2/pointer.cpp
+++ IF-B/Pertemuan_2/pointer.cpp
@@ -2,32 +2,38 @@
 
 using namespace std;
 
-
-void nullref(int*& p1){ // bukan p1 sama seperti di main
+void nullref(int *&p1)
+{ // bukan p1 sama seperti di main
     cout << *p1 << endl;
     p1 = nullptr;
-    if(p1 == nullptr){
+    if (p1 == nullptr)
+    {
         cout << "null pointer reference" << endl;
     }
-    else{
+    else
+    {
         cout << *p1 << endl;
     }
 }
 
-void changeval(int& ref, int val){
+void changeval(int &ref, int val)
+{
     ref = val;
     cout << ref << endl;
 }
 
-int main(){
+int main()
+{
     int val = 150;
-    int* ref = &val;
+    int *ref = &val;
     cout << *ref << endl;
     nullref(ref);
-    if(ref == nullptr){
+    if (ref == nullptr)
+    {
         cout << "null pointer reference" << endl;
     }
-    else{
+    else
+    {
         cout << *ref << endl;
     }
 }
