8b3a60aec6bf2b29469323dfd7d7f53a85379fb3
Morxidia <xakyazx@gmail.com>
2026-09-21 14:37:20 +0700

    if-b pertemuan 4

--- IF-B/pertemuan_4/linkedList.cpp
+++ IF-B/pertemuan_4/linkedList.cpp
@@ -7,14 +7,14 @@ struct Mahasiswa
     std::string nim;
     std::string nama;
     double ipk;
+    Mahasiswa *prev;
     Mahasiswa *next;
 };
 
-bool nimTersedia(Mahasiswa *head, std::string nim);
-bool tambahMahasiswa(Mahasiswa *&head,
-                     string nama, string nim, double ipk);
 Mahasiswa *cariNim(Mahasiswa *head, std::string nim);
-bool hapusNim(Mahasiswa *&head, std::string nim);
+bool nimTersedia(Mahasiswa *head, std::string nim);
+bool tambahMahasiswa(Mahasiswa *&head, Mahasiswa *&tail, string nama, string nim, double ipk);
+bool hapusNim(Mahasiswa *&head, Mahasiswa *&tail, std::string nim);
 void tampilkan(Mahasiswa *head);
 int jumlahData(Mahasiswa *head);
 void clear(Mahasiswa *&head);
@@ -48,14 +48,14 @@ void tampilkan(Mahasiswa *head)
 int main()
 {
     Mahasiswa *head = nullptr;
+    Mahasiswa *tail = nullptr;
     int pilihan;
 
-    cout << tambahMahasiswa(head, "taff", "12301", 2.3) << endl;
-    cout << tambahMahasiswa(head, "taff", "12303", 2.3) << endl;
-    cout << tambahMahasiswa(head, "taff", "12302", 2.3) << endl;
-    cout << tambahMahasiswa(head, "taff", "12301", 2.3) << endl;
-    cout << hapusNim(head, "12302") << endl;
-    cout << "test" << endl;
+    cout << tambahMahasiswa(head, tail, "taff", "12301", 2.3) << endl;
+    cout << tambahMahasiswa(head, tail, "taff", "12310", 2.9) << endl;
+    cout << tambahMahasiswa(head, tail, "taff", "12305", 2.7) << endl;
+    cout << tambahMahasiswa(head, tail, "taff", "12307", 3.3) << endl;
+    cout << (hapusNim(head, tail, "12305") ? "berhasil dihapus" : "gagal dihapus") << endl;
     tampilkan(head);
     // clear(head);
     return 0;
@@ -86,8 +86,7 @@ Mahasiswa *cariNim(Mahasiswa *head, std::string nim)
     }
 }
 
-bool nimTersedia(Mahasiswa *head,
-                 std::string nim)
+bool nimTersedia(Mahasiswa *head, std::string nim)
 {
     if (cariNim(head, nim) == nullptr)
     {
@@ -99,8 +98,7 @@ bool nimTersedia(Mahasiswa *head,
     }
 }
 
-bool tambahMahasiswa(Mahasiswa *&head,
-                     string nama, string nim, double ipk)
+bool tambahMahasiswa(Mahasiswa *&head, Mahasiswa *&tail, string nama, string nim, double ipk)
 {
     if (nimTersedia(head, nim) == false)
     {
@@ -112,42 +110,64 @@ bool tambahMahasiswa(Mahasiswa *&head,
     NodeBaru->nim = nim;
     NodeBaru->ipk = ipk;
     NodeBaru->next = nullptr;
+    NodeBaru->prev = nullptr;
 
+    // sisip depan ketika list Kosong/ initialisasi
     if (head == nullptr)
     {
         head = NodeBaru;
+        tail = NodeBaru;
         return true;
     }
     else
     {
         Mahasiswa *temp = head;
+
+        // sisip depan ketika list beranggota
         if (temp->nim > nim)
         {
             NodeBaru->next = temp;
+            head->prev = NodeBaru;
             head = NodeBaru;
         }
+
+        // sisip tengah/akhir lakukan traverse
         while (temp->next != nullptr && temp->next->nim < nim)
         {
             temp = temp->next;
         }
-        NodeBaru->next = temp->next;
-        temp->next = NodeBaru;
+
+        // alternative condition (temp == tail)
+        if (temp->next == nullptr)
+        {
+            // kondisi ketika sisip belakang
+            temp->next = NodeBaru;
+            NodeBaru->prev = temp;
+            tail == NodeBaru;
+        }
+        else
+        {
+            // kondisi ketika sisip tengah
+            temp->next->prev = NodeBaru;
+            NodeBaru->next = temp->next;
+            temp->next = NodeBaru;
+            NodeBaru->prev = temp;
+        }
         return true;
     }
     return true;
 }
 
-bool hapusNim(Mahasiswa *&head, std::string nim)
+bool hapusNim(Mahasiswa *&head, Mahasiswa *&tail, std::string nim)
 {
     if (head == nullptr)
     {
         return false;
     }
     Mahasiswa *currNode = head;
-    Mahasiswa *sebelum = nullptr;
+
     while (currNode->nim != nim && currNode->next != nullptr)
     {
-        sebelum = currNode;
         currNode = currNode->next;
     }
 
@@ -159,19 +179,21 @@ bool hapusNim(Mahasiswa *&head, std::string nim)
     {
         if (currNode == head)
         {
+            // hapus depan, head maju selangkah, hapus current
             head = currNode->next;
-            currNode->next = nullptr;
+            head->prev = nullptr;
         }
         else if (currNode != head && currNode->next != nullptr)
         {
-            Mahasiswa *sesudah = currNode->next;
-            sebelum->next = sesudah;
-            currNode->next = nullptr;
+            // hapus tengah
+            currNode->prev->next = currNode->next;
+            currNode->next->prev = currNode->prev;
         }
         else
         {
-            sebelum->next = nullptr;
-            currNode->next = nullptr;
+            // hapus akhir
+            tail = currNode->prev;
+            tail->next = nullptr;
         }
         delete currNode;
         return true;
